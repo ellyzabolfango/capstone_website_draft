602010b1189011c565969c5b9a152455e277004d
ellyzabolfango <167157580+ellyzabolfango@users.noreply.github.com>
2025-08-31 08:59:19 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
+// /public/index.php
 session_start();
-if (empty($_SESSION['user_id'])) {
-  header("Location: /capstone_website/public/login.php");
-  exit();
-}
 require_once __DIR__ . '/../helpers/auth.php';
+
+// force login if not authenticated
+auth_required();
+
+// if logged in, send them to the right dashboard
 redirect_by_role();
